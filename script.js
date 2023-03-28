fef46a6fd2205fd0efdf4c9942c221d3a46cb263
tim-iz-kgstana88 <81582790+tim-iz-kgstana88@users.noreply.github.com>
2023-03-28 20:17:13 +0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,28 @@
+
+$(function () {
+
+    $("#typing").typed({
+        strings: ["6А - класс пред", "ээ не не так", "лучше напишу неудачные кадры", "Долго споря, мы пришли к нашему номеру - выступлению тоже ручной работы..."],
+        typeSpeed: 100,
+        //backDelay: 1500,
+        //startDelay: 2500,
+        loop: true,
+        loopCount: 2,
+        contentType: 'html',
+    });
+    
+
+});
+// $(function(){
+
+//     $("#typing").typed({
+//         strings: [" В Африке гориллы.", " В Африке большие.", " Злые крокодилы."],
+//         typeSpeed: 70,
+//         backDelay: 1500,
+//         startDelay: 2500,
+//         loop: true,
+//         loopCount: 2,
+//         contentType: 'html',      
+//     });
+
+// });
